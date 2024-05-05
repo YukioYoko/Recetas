8d286883728b4f93e3bacbfec4e4d525f8d33235
FM <152152436+Fallop@users.noreply.github.com>
2024-05-05 15:10:14 -0600

    Commit creacion inicial Home
    home inicial

---
+++ client/src/components/ui/Button.jsx
@@ -0,0 +1,11 @@
+export function Button(props) {
+  return (
+    <button>
+        className="flex flex-col justify-center items-center px-4 py-6 rounded-lg flex-grow-0"
+        {...props}
+    </button> 
+  )
+}
+
+
+export default Button;
