8215975d54cf021da82b74d0c0af069eb575cf5a
KinderBueno <letp0111@gmail.com>
2024-05-06 19:36:50 -0600

    Cartas de coleccion y pagina de coleccion terminada
    Listoki

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,23 @@ import { RecipePage } from './pages/RecipePage'
 import { RecipeFormPage } from './pages/RecipeFormPage'
 import { Navigation } from './components/Navigation'
 import { Toaster } from "react-hot-toast"
-import { CardsColecciones } from './components/CardsColecciones'
 import { HomePage } from './pages/HomePage'
+import { Colecciones } from './pages/Colecciones'
 
 function App(){
   return (
     <BrowserRouter>
     <div className=' bg-custom-naranja-logo min-h-screen min-w-screen'>
       <Navigation/>
-
       <Routes>
       <Route path="/" element={<HomePage/>} />
         <Route path="/recipes" element={<RecipePage/>} />
         <Route path="/recipes-create" element={<RecipeFormPage/>} />
         <Route path="/recipes/:id" element={<RecipeFormPage/>} />
+        <Route path="/colecciones" element={<Colecciones/>} />
       </Routes>
       <Toaster/>
-      <CardsColecciones/>
-    
+      
 
       
     </div>
