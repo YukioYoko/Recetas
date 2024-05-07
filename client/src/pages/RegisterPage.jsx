d91142be970dbf8ec8d4e0618efe4ebd95c9b3b3
YukioYoko <yukio.yokogawa141002@gmail.com>
2024-05-06 21:39:31 -0600

    un poco css

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -4,47 +4,50 @@ import { Navigation } from '../components/NavLog';
 
 export function RegisterPage() {
   return (
-    <div>
-      <div>
+    <div class="grid grid-cols-2 gap 4">
+      <div class="flex items-center content-center flex-col ">
         <img src={logo} className='w-28'/>
         <h2>Que estas esperando?</h2>
         <h3>Sube, guarda y cocina lo que mas te guste</h3>
       </div>
-      <div class="FormRegistro">
+      <div className="FromRegistro" class="flex justify-center items-center flex-col " >
         <form action="">
-          <div class='Nombre'>
+          <div className='Nombre' class="flex flex-col pt-5 pr-10 pl-5">
             <label htmlFor="">NOMBRE(S)</label>
             <input type="text" name="nombre" placeholder='Ingresa tu(s) nombre(s)'/>
           </div>
-          <div class='Apellidos'>
+          <div className='Apellidos' class="flex flex-col pt-3 pr-10 pl-5">
             <label htmlFor="">APELLIDOS</label>
             <input type="text" name="apellido" placeholder='Ingresa tus apellidos'/>
           </div>
-          <div class='Correo'>
+          <div className='Correo' class="flex flex-col pt-3 pr-10 pl-5">
             <label htmlFor="">CORREO</label>
             <input type="email" name="correo" placeholder='Ingresa tu correo electronico'/>
           </div>
-          <div class='Contrasena'>
+          <div className='Contrasena' class="flex flex-col pt-3 pr-10 pl-5">
             <label htmlFor="">CONTRASENA</label>
             <input type="text" name="contrasena" placeholder='Ingresa tu contrasena'/>
           </div>
-          <div class='FechaNac'>
+          <div className='FechaNac' class="flex flex-col pt-3 pr-10 pl-5">
             <label for="fecha">Selecciona una fecha:</label>
             <input type="date" id="fecha" name="fecha"/>
           </div>
-          <div class='TerminosCond'>
+          <div className='TerminosCond' class="pt-3 pr-10 pl-5">
             <label for="terminos">
             <input type="checkbox" id="terminos" name="terminos"/>
             Acepto los términos y condiciones
             </label>
           </div>
-          <div class='SendButton'>
-              <input type="submit" value="Registrarme"/>
+          <div class="flex justify-center items-center flex-row pl-4 pt-7">
+            <div class='SendButton'>
+                <input type="submit" value="Registrarme"/>
+            </div>
+            <div class="pl-8"></div>
+            <div>
+              <a href="#">Ya tengo una cuenta</a>
+            </div>
           </div>
         </form>
-      </div>
-      <div>
-        <a href="#">Ya tengo una cuenta</a>
       </div>
     </div>
   )
