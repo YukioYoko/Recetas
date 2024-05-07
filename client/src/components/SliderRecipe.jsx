017316b2f7606af8d44c02286a8dd9f5626da365
FM <152152436+Fallop@users.noreply.github.com>
2024-05-06 22:52:29 -0600

    Diseño Inicial Pagina Home
    Diseño inicial de la pagina home terminado

--- client/src/components/SliderRecipe.jsx
+++ client/src/components/SliderRecipe.jsx
@@ -1,11 +1,20 @@
 import { redirectDocument, useNavigate } from "react-router-dom"
 import { SliderRecipeCard } from './SliderRecipeCard'
+import { Link } from 'react-router-dom'
 
-export function SliderRecipe({recipe}) {
+export function SliderRecipe() {
 
     return (
-        <div className="bg-custom-beige p-3">
-            <SliderRecipeCard/>
+        <div className="flex p-3 gap-6 mb-12 justify-center">
+            <Link to="/recipe">
+                <SliderRecipeCard/>
+            </Link>
+            <Link to="/recipe">
+                <SliderRecipeCard/>
+            </Link>
+            <Link to="/recipe">
+                <SliderRecipeCard/>
+            </Link>
         </div>
     )
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import { SliderRecipe } from '../components/SliderRecipe'
 
+
 export function HomePage() {
   return (
-    <div className="font-title text-[24px] text-custom-beige p-6">
+    <div className="font-title text-[64px] text-custom-beige p-6">
       <h2 className=''>Destacadas</h2>
       <SliderRecipe/>
       <h2>Recetas Mejor Valoradas</h2>
