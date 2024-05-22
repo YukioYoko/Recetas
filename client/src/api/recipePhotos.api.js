947b7719a973c256e31e0ee2cd99c2a517a9104a
FM <152152436+Fallop@users.noreply.github.com>
2024-05-21 19:30:45 -0600

    Pagina de Agregar Recetas Terminada

--- client/src/api/recipePhotos.api.js
+++ client/src/api/recipePhotos.api.js
@@ -6,6 +6,17 @@ const recipePhotosApi = axios.create({
 
 export const getAllPhotos= () =>  recipePhotosApi.get('/')
 export const getRecipePhoto = (id) =>  recipePhotosApi.get(`/${id}/`)
-export const createRecipePhoto = (recipePhoto) => recipePhotosApi.post('/', recipePhoto)
 export const deleteRecipePhoto = (id) => recipePhotosApi.delete(`/${id}`)
 export const updateRecipePhoto = (id, recipe) => recipePhotosApi.put(`/${id}/`, recipePhoto)
+
+export const createRecipePhoto = (recipePhoto) => {
+    const formData = new FormData();
+    formData.append('recipe', recipePhoto.recipe);
+    formData.append('photo', recipePhoto.photo);
+
+    return recipePhotosApi.post('/', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    });
+};
